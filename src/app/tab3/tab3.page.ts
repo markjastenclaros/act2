e54bff3b93f2f860f275a885496300d6db831c8b
markjastenclaros <markjasten.clarosiii@evsu.edu.ph>
2021-04-29 13:19:23 +0700

    commit this

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { ContactServiceService } from '../services/contact-service.service';
+import {ToastController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
+
 export class Tab3Page {
+  contName ="";
+  contNumber ="";
+  contacts: any =[];
 
-  constructor() {}
+  constructor(public contactService: ContactServiceService, public toast: ToastController){}
 
+  async ngOnInit(){
+    this.contactService.getData().then(data=> {
+        this.contacts = data;
+  });
+  }
+
+  saveC(){
+    this.contactService.saveContact({name: this.contName,number:this.contNumber}).then(data=>{
+      this.contacts =data;
+      this.clearField()
+
+    });
+  }
+
+  clearField(){
+    this.contName ="";
+    this.contNumber ="";
+  }
+
+  
 }
+
+  
+
+
+
+
+
+
